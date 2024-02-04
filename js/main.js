9aa0d4b356399fe605377f4eec41a3b8b6786b67
Cory <corys4139@gmail.com>
2024-02-04 11:22:05 -0500

    add photos and location

--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
 // event listeners
-const submitBtn = document.getElementById("checkDay");
+const submitBtn = document.getElementById("scroll");
+const locationBtn = document.getElementById("location");
 const resetBtn = document.getElementById("reset");
 const text = document.getElementById("textDiv");
 const container = document.getElementById("containerDiv");
 const img = document.getElementById("imgDiv");
 
-const daysOfWeek = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday",
-];
+let photoNum = 0;
 
 text.innerHTML = "";
 container.innerHTML = "";
 img.innerHTML = "";
 
 submitBtn.addEventListener("click", () => {
-	today = new Date();
-	dayToday = today.getDay();
-	container.innerHTML = daysOfWeek[dayToday];
-	if (dayToday === 5) {
-		text.innerText = "Yes. It is:";
-		img.innerHTML = '<img class="reaction" src="/images/yes.jpg">';
-        confetti.start(2000, ["red", "#FFFFFF", "cornflowerblue", "pink", "green", "orange", "purple"]);
-	} else {
-		text.innerText = "No. It is:";
-		img.innerHTML = '<img class="reaction" src="/images/no.jpg">';
+	if (photoNum != 3) {
+		photoNum = photoNum + 1
+		img.innerHTML = '<img class="reaction" src="/images/wedding/' + photoNum + '.jpeg">';
 	}
+	else {
+		photoNum = 1
+		img.innerHTML =
+			'<img class="reaction" src="/images/wedding/' + photoNum + '.jpeg">';
+	}
+});
+
+locationBtn.addEventListener("click", () => {
+	img.innerHTML =
+		'<img class="reaction" src="/images/wedding/king_umberto.jpeg">';
+	text.innerText = "King Umberto, 1343 Hempstead Turnpike, Elmont, NY 11003";
 });
 
 resetBtn.addEventListener("click", () => {
